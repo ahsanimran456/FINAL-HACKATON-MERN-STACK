ab15f27983f1889e78d47ff7f8727d45a6accf8e
unknown <ahxanimran456@gmail.com>
2023-02-11 12:06:11 +0500

    update

--- Backend-Server/index.js
+++ Backend-Server/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from "cors"
 
-const port = process.env.PORT
+const Port = process.env.PORT || 50552
 const MAngodb = process.env.MANGODBLINK
 const app = express()
 
@@ -13,3 +13,13 @@ dotenv.config()
 mongoose.connect(`mongodb+srv://ahsanimran:saylanihackaton@cluster0.6s824ks.mongodb.net/FinalHackaton?retryWrites=true&w=majority`)
     .then(() => console.log("Connected !!"))
     .catch((err) => console.log("err ===>", err))
+
+
+
+app.use("/", (req, res) => {
+    res.send(new Date());
+});
+
+app.listen(Port, () => {
+    console.log(`server is running on port!${Port}`);
+});   
