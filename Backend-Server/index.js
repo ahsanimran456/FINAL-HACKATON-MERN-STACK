f49a5edc6aed5f1ab12d2c949a98fb732ff6026b
unknown <ahxanimran456@gmail.com>
2023-02-11 11:57:25 +0500

    update

--- Backend-Server/index.js
+++ Backend-Server/index.js
@@ -4,5 +4,12 @@ import dotenv from 'dotenv';
 import cors from "cors"
 
 const port = process.env.PORT
+const MAngodb = process.env.MANGODBLINK
+const app = express()
 
-dotenv.config() 
+app.use(express.json());
+dotenv.config()
+
+mongoose.connect(`mongodb+srv://ahsanimran:saylanihackaton@cluster0.6s824ks.mongodb.net/FinalHackaton?retryWrites=true&w=majority`)
+    .then(() => console.log("Connected !!"))
+    .catch((err) => console.log("err ===>", err))
