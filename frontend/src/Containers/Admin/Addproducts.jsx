0ac3d5bd3431a477c231fd2c8af797e89a2d37c3
unknown <ahxanimran456@gmail.com>
2023-02-12 20:05:06 +0500

    update

--- frontend/src/Containers/Admin/Addproducts.jsx
+++ frontend/src/Containers/Admin/Addproducts.jsx
@@ -2,8 +2,72 @@ import "./Admin.css"
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import { useState } from "react";
+import { async } from "@firebase/util";
+import {
+    getAuth,
+    doc,
+    setDoc,
+    db,
+    onAuthStateChanged,
+    getDocs,
+    getDoc,
+    collection, auth,
+    onSnapshot,
+    query, where,
+    addDoc,
+    orderBy
+} from '../../FirebaseConfig/Firebase.js'
+import CameraAltIcon from '@mui/icons-material/CameraAlt'
+import { Upload } from 'antd';
+import ImgCrop from 'antd-img-crop';
+
 function AddProducts() {
+    const [itemname, setitemname] = useState();
     const [category, setcategory] = useState();
+    const [description, setdescription] = useState();
+    const [unitname, setunitname] = useState();
+    const [unitprice, setunitprice] = useState();
+
+    const [fileList, setFileList] = useState([]);
+
+    const onChange = ({ fileList: newFileList }) => {
+        setFileList(newFileList);
+    };
+    const onPreview = async (file) => {
+        let src = file.url;
+        if (!src) {
+            src = await new Promise((resolve) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(file.originFileObj);
+                reader.onload = () => resolve(reader.result);
+            });
+        }
+    }
+    const uploadButton = (
+        <div className='uplaoder_imag' >
+            <CameraAltIcon className='camera_icon' />
+        </div>
+    )
+
+    const addproduct = async () => {
+        console.log(itemname, category, description, unitname, unitprice)
+        try {
+            await addDoc(collection(db, "Products"), {
+                itemname: itemname,
+                category: category,
+                description: description,
+                unitname: unitname,
+                unitprice: unitprice,
+                timestamp: new Date(),
+            })
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
+
     return (
         <div className="Addproducts">
             <div>
@@ -15,8 +79,20 @@ function AddProducts() {
                 </h3>
             </div>
             <div className="additemsinputs">
+                <dir>
+                    <ImgCrop rotate>
+                        <Upload
+                            listType="picture-card"
+                            fileList={fileList}
+                            onChange={onChange}
+                            onPreview={onPreview}
+                        >
+                            {fileList.length < 1 && uploadButton}
+                        </Upload>
+                    </ImgCrop>
+                </dir>
                 <div className="eachinputs">
-                    <input type="text" placeholder="Item Name" />
+                    <input type="text" placeholder="Item Name" onChange={(e) => setitemname(e.target.value)} />
                 </div>
                 <div className="eachinputs">
                     <select value={category} onChange={(e) => setcategory(e.target.value)} className="slectsinputs" >
@@ -28,18 +104,18 @@ function AddProducts() {
                     </select>
                 </div>
                 <div className="eachinputs">
-                    <textarea name="" id="" cols="30" rows="5" className="slectsinputs"></textarea>
+                    <textarea name="" id="" cols="30" rows="5" className="slectsinputs" onChange={(e) => setdescription(e.target.value)}></textarea>
                 </div>
                 <div className="eachinputs">
                     <span style={{ color: "#024F9D", fontWeight: 700 }}>Unit Name: </span>
-                    <input type="text" placeholder="Item Name" />
+                    <input type="text" placeholder="Unit Name KG/DOZEN" onChange={(e) => setunitname(e.target.value)} />
                 </div>
                 <div className="eachinputs">
-                    <span style={{ color: "#024F9D", fontWeight: 700 }}>Unit Name: </span>
-                    <input type="text" placeholder="Item Name" />
+                    <span style={{ color: "#024F9D", fontWeight: 700 }}>Unit Price: </span>
+                    <input type="number" placeholder="Price" onChange={(e) => setunitprice(e.target.value)} />
                 </div>
                 <div>
-                    <button className="addproductbtn">
+                    <button className="addproductbtn" onClick={addproduct}>
                         <span>
                             Add Product
                         </span>
