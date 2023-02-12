bb4d037d3e901a7f5688702ebc84cf5003447f9a
unknown <ahxanimran456@gmail.com>
2023-02-12 17:49:49 +0500

    update

--- frontend/src/Components/Cart.jsx
+++ frontend/src/Components/Cart.jsx
@@ -1,5 +1,41 @@
+import './Component.css'
+import {
+    PlusOutlined
+} from '@ant-design/icons'
 function Cart() {
-    return (  );
+    return (
+        <div className="cartmain">
+            <div className="eachproduct">
+                <div>
+                    <img src={require("../assests/images/item4.jpg")} alt="" width={50} height={60} style={{ borderRadius: 7 }} />
+                </div>
+                <div>
+                    <div className='cartpriceheader'>
+                        <div>
+                            <h4>
+                                Meat
+                            </h4>
+                        </div>
+                        <div>
+                            850 RS
+                        </div>
+                    </div>
+                    <div>
+                        <p>
+                            product discription achi hy
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <button className='addtocart'>
+                        <span>
+                            <PlusOutlined style={{ fontSize: 20, color: "#fff", fontWeight: 700,marginTop:-5 }} />
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Cart;
